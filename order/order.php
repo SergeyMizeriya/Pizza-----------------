b144c195d0b9692e6090dac21c11ecbcaa70fd28
SergeyMizeriya <pixmaqwerty@mail.ru>
2022-07-13 14:01:14 +0300

    planing

--- order/order.php
+++ order/order.php
@@ -4,3 +4,6 @@ require_once('./classes/Pizza.php');
 require_once('./classes/FinalPrice.php');
 
 print_r($_POST);
+//сюда приходит AJAX запрос с данными формы
+// делаем пиццу new AnyPizza()
+// метод от AnyPizza возвращает цены на пиццу, размер и соус(ы)
